92d74bf60e58e407d1dcbeb727eaf960a4318b4b
omaks45 <danielgozie5@gmail..com>
2026-05-26 10:48:10 +0100

    prisma service updated

---
+++ src/common/prisma/prisma.service.ts
@@ -0,0 +1,35 @@
+import {
+    Injectable,
+    Logger,
+    OnModuleDestroy,
+    OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy
+    {
+    private readonly logger = new Logger(PrismaService.name);
+
+    constructor() {
+        super({
+        log: [
+            { emit: 'event', level: 'query' },
+            { emit: 'stdout', level: 'error' },
+            { emit: 'stdout', level: 'warn' },
+        ],
+        });
+    }
+
+    async onModuleInit(): Promise<void> {
+        await this.$connect();
+        this.logger.log('Database connection established');
+    }
+
+    async onModuleDestroy(): Promise<void> {
+        await this.$disconnect();
+        this.logger.log('Database connection closed');
+    }
+}
