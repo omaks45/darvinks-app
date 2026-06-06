8fd4a8c5ea19e0ddc228bf1525b423b7e50da56b
omaks45 <danielgozie5@gmail..com>
2026-06-06 13:19:56 +0100

    user module file updated

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,15 @@
 // src/modules/user/user.module.ts
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
 import { UsersController } from './user.controller';
 import { UsersService } from './user.service';
 import { CloudinaryModule } from '@modules/cloudinary/cloudinary.module';
 
 @Module({
-  imports: [CloudinaryModule],
+  imports: [
+    CloudinaryModule,
+    BullModule.registerQueue({ name: 'notifications' }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
