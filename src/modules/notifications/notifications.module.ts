1391a94acc168071487118b42e4a628efd681f33
omaks45 <danielgozie5@gmail..com>
2026-06-06 13:56:17 +0100

    notification module updated

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,19 +1,18 @@
-// src/modules/notifications/notifications.module.ts
+
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
 import { NotificationsProcessor } from './notifications.processor';
 import { IdCardWorker } from './workers/id-card.worker';
+import { MailModule } from '@modules/email/email.module';
 import { CloudinaryModule } from '@modules/cloudinary/cloudinary.module';
 import { PrismaModule } from '@common/prisma/prisma.module';
-
-// UsersModule intentionally NOT imported here — IdCardWorker uses
-// PrismaService directly to avoid module path inconsistencies
 
 @Module({
   imports: [
     BullModule.registerQueue({ name: 'notifications' }),
     PrismaModule,
     CloudinaryModule,
+    MailModule,
   ],
   providers: [NotificationsProcessor, IdCardWorker],
   exports: [IdCardWorker],
