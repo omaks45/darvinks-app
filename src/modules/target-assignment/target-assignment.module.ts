3a2836456f1488aa203cfc40c93eb3c427bc55e0
omaks45 <danielgozie5@gmail..com>
2026-08-14 12:56:41 +0100

    target module updated

--- src/modules/target-assignment/target-assignment.module.ts
+++ src/modules/target-assignment/target-assignment.module.ts
@@ -2,8 +2,10 @@
 import { Module } from '@nestjs/common';
 import { TargetAssignmentController } from './target-assignment.controller';
 import { TargetAssignmentService } from './target-assignment.service';
+import { NotificationsModule } from '@modules/notifications/notifications.module';
 
 @Module({
+  imports:     [NotificationsModule],
   controllers: [TargetAssignmentController],
   providers:   [TargetAssignmentService],
   exports:     [TargetAssignmentService],
